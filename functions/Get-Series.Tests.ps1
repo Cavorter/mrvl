ed518763eb27b1f73da799f61b63f7c71b48feaa
Nathan Stohlmann <nathan@virtualmicropolis.com>
2022-08-14 12:35:58 -0500

    Added sample objects for tests. Added tests for Get-Series.

--- functions/Get-Series.Tests.ps1
+++ functions/Get-Series.Tests.ps1
@@ -1,9 +1,51 @@
 BeforeAll {
-    . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+    $setupScript = Join-Path -Path $PSScriptRoot -ChildPath _setupTests.ps1
+    . $setupScript
 }
 
+
 Describe "Get-Series" {
-    It "Returns expected output" {
-        Get-Series | Should -Be "YOUR_EXPECTED_VALUE"
+    BeforeDiscovery {
+        $nameWildcardCases = @( '*' , '?' )
+        $idContentCases = @('Characters', 'Comics', 'Creators', 'Events', 'Stories')
+    }
+
+    BeforeAll {
+        $mockBase = @{
+            ModuleName  = $moduleName
+            CommandName = "Invoke-Api"
+        }
+        Mock @mockBase -MockWith { return $testSeriesData }
+
+        $invokeParams = $mockBase + @{
+            Invoke  = [switch]$true
+            Times   = 1
+            Exactly = [switch]$true
+        }
+
+        $goodName = $testSeriesObj.Title
+    }
+
+    It "Searches for a series by name without a wildcard" {
+        Get-MarvelSeries -Name $goodName | Out-Null
+        Should @invokeParams -ParameterFilter { $Path -eq @( "series" ) -and $Query.title -eq $goodName -and $Query.Keys.Count -eq 1 }
+    }
+
+    It "Searches for a series by name with a <_> wildcard" -TestCases $nameWildcardCases {
+        Get-MarvelSeries -Name ( $goodName + $_ ) | Out-Null
+        Should @invokeParams -ParameterFilter { $Path -eq @( "series" ) -and $Query.titleStartsWith -eq $goodName -and $Query.Keys.Count -eq 1 }
+    }
+
+    It "Retrieves a series by Id" {
+        $goodId = 98765
+        Get-MarvelSeries -Id $goodId | Out-Null
+        Should @invokeParams -ParameterFilter { $Path[0] -eq "series" -and $Path[1] -eq $goodId -and $Query.Keys.Count -eq 0 }
+    }
+
+    It "Retrieves a series by Id and content type <_>" -TestCases $idContentCases {
+        $contentType = $_
+        $goodId = 98765
+        Get-MarvelSeries -Id $goodId -Content $contentType | Out-Null
+        Should @invokeParams -ParameterFilter { $Path[0] -eq "series" -and $Path[1] -eq $goodId -and $Path[2] -eq $contentType -and $Query.Keys.Count -eq 0 }
     }
 }
--- functions/_setupTests.ps1
+++ functions/_setupTests.ps1
@@ -5,3 +5,10 @@ $manifestFile = Get-ChildItem -Path $manifestPath
 $moduleName = $manifestFile.BaseName
 
 Import-Module $manifestFile.FullName -Force
+
+$contentPath = Join-Path -Path $PSScriptRoot -ChildPath test-objects -AdditionalChildPath "*.json"
+foreach ( $item in ( Get-ChildItem -Path $contentPath )) {
+    $dataVarName = "test$( $item.BaseName )Data"
+    New-Variable -Scope Local -Name $dataVarName -Value ( Get-Content -Path $item.FullName | ConvertFrom-Json )
+    New-Variable -Scope Local -Name "test$( $item.BaseName )Obj" -Value ( [MarvelSeries]( Get-Variable -Scope Local -Name $dataVarName ).Value )
+}
